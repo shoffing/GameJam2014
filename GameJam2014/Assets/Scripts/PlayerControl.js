b0e1b21e6bad042f3ae92297e7e12bf23bc17ade
Sean Canuel <seancanuel@gmail.com>
2014-01-26 00:50:17 -0400

    Scene Erased Mostly, Movement stuff

--- GameJam2014/Assets/Scripts/PlayerControl.js
+++ GameJam2014/Assets/Scripts/PlayerControl.js
@@ -16,18 +16,57 @@ function Update() {
 	if(chMotor.grounded && chCont.velocity.magnitude == 0) {
 		lastSafePosition = transform.position;
 	}
-	if( chCont.velocity.magnitude < 3 ){
-		anger = anger * 0.95;
+	if( chCont.velocity.magnitude < 4 ){
+		anger = anger - 0.2;
 		if( anger < 1 ){
 			anger = 0;
 		}
 	}
 	else{
-		anger = anger + 0.3 ;
+		anger = anger + 0.4 ;
 		if( anger > 100 ){
 			anger = 100;
 		}
 	}
+	var move = chMotor.movement;
+	var tier = Mathf.Floor(anger/20);
+	// Tiers of angryness, changing the movement stuff
+	if( tier == 1 ){ // Anger 1
+		move.maxForwardSpeed = 9;
+		move.maxGroundAcceleration = 12;
+		Debug.Log("Anger 1");
+	}
+	else if( tier == 2 ){ // Anger 2
+		move.maxForwardSpeed = 13;
+		move.maxGroundAcceleration = 14;
+		Debug.Log("Anger 2");
+	}
+	else if( tier == 3 ){ // Anger 3
+		move.maxForwardSpeed = 17;
+		move.maxGroundAcceleration = 16;
+		Debug.Log("Anger 3");
+	}
+	else if( tier == 4 ){ // SUPER ANGRY
+		move.maxForwardSpeed = 21;
+		move.maxGroundAcceleration = 18;
+		Debug.Log("Anger 4");
+	}
+	else if( tier == 5 ){ // MAXIMUM ANGER
+		move.maxForwardSpeed = 25;
+		move.maxGroundAcceleration = 20;
+		Debug.Log("Anger MAX");
+	}
+	else{ // Calm
+		move.maxForwardSpeed = 5;
+		move.maxGroundAcceleration = 10;
+		move.maxSidewaysSpeed = 5;
+		move.maxBackwardsSpeed = 5;
+		Debug.Log("Calm");
+	}
+	
+	
+	// Camera Control stuff
+	GameObject.FindWithTag("MainCamera");
 	
 }
 
