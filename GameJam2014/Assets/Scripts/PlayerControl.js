b2fcc824a4e40e39b2b7c1a32fb8c7ef91e17223
shoffing <shoffing@gmail.com>
2014-01-25 20:04:08 -0500

    initial stuff

---
+++ GameJam2014/Assets/Scripts/PlayerControl.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+private var chCont : CharacterController;
+private var chMotor : CharacterMotor;
+private var lastSafePosition : Vector3;
+
+function Start() {
+	chMotor = GetComponent(CharacterMotor);
+	chCont = GetComponent(CharacterController);
+	
+	lastSafePosition = transform.position;
+}
+
+function Update() {
+	if(chMotor.grounded && chCont.velocity.magnitude == 0) {
+		lastSafePosition = transform.position;
+	}
+}
+
+function ResetPlayer() {
+	transform.position = lastSafePosition;
+	chMotor.SetVelocity(Vector3.zero);
+}
