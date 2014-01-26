6cd94470a437489630636cd207b23498b9515b6e
Charlie <kiddbond@yahoo.com>
2014-01-26 11:45:14 -0500

    Music Script
    The beginning of a music controller.
    
    Resource Folder is used when game is compiled or whatever.

---
+++ GameJam2014/Assets/Scripts/MusicController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicController : MonoBehaviour {
+
+	private static AudioClip current;
+	private static AudioClip[] songs;
+	private Dictionary<string, AudioClip> songList;
+	private AudioSource musicController;
+
+
+	private void _ConvertArrayToDict()
+	{
+		foreach( AudioClip clip in songs )
+		{
+			songList.Add( clip.name, clip );
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		musicController = GetComponent<AudioSource>();
+		songs = Resources.LoadAll<AudioClip>("Music");
+		_ConvertArrayToDict();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlaySong( string song, bool loop )
+	{
+		AudioClip newSong;
+		if( songList.TryGetValue( song, out newSong ) )
+		{
+			current = newSong;
+			musicController.clip = current;
+			musicController.loop = loop;
+		}
+	}
+
+	void PauseSong()
+	{
+		musicController.Pause();
+	}
+
+	void StopSong()
+	{
+		musicController.Stop();
+	}
+
+	void FadeInSong( string song, bool loop )
+	{
+
+	}
+
+	public static AudioClip CurrentSong()
+	{
+		return current;
+	}
+}
